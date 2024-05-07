2e37bdeac54168e9dba1f8d0bfe9193700ef1ee2
Ivan <120404084+altervee@users.noreply.github.com>
2024-05-07 20:45:10 +0200

    Fitht commit
    This tiem I added herats(or lives) and damage for my player, furthermore if the lives are 0 we start again

--- SAM HELLSING/Assets/Scripts/GameManager.cs
+++ SAM HELLSING/Assets/Scripts/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager Instance { get; private set; }
+    public HUD hud;
     public int PuntosTotales { get; private set; }
     //private int puntosTotales;
+    private int vidas = 5;
 
     private void Awake()
     {
@@ -20,16 +23,40 @@ public class GameManager : MonoBehaviour
             Debug.Log("Aviso! Máss de un GameManager en escena.");
         }
     }
-    public void SumarPuntos(int puntosSumar)
+    public void SumarPuntos(int puntosASumar)
     {
-        PuntosTotales += puntosSumar; // Actualizar la propiedad PuntosTotales
-        Debug.Log(PuntosTotales); // Visualizar los puntos totales
+        PuntosTotales += puntosASumar;
+        hud.UpdatePoints(PuntosTotales);
     }
+
     void Start()
     {
         
     }
+    public void PerderVida()
+    {
+        vidas -= 1;
 
+        if (vidas == 0)
+        {
+            // Reiniciamos el nivel.
+            SceneManager.LoadScene(0);
+        }
+
+        hud.FinishHeart(vidas);
+    }
+
+    public bool RecuperarVida()
+    {
+        if (vidas == 5)
+        {
+            return false;
+        }
+
+        hud.EarntHeart(vidas);
+        vidas += 1;
+        return true;
+    }
     // Update is called once per frame
     void Update()
     {
--- SAM HELLSING/Assets/Scripts/HUD.cs
+++ SAM HELLSING/Assets/Scripts/HUD.cs
@@ -7,6 +7,7 @@ public class HUD : MonoBehaviour
 {
     
     public TextMeshProUGUI puntos;
+    public GameObject[] heart;// colleccion de varios objetos
     void Start()
     {
         
@@ -16,4 +17,20 @@ public class HUD : MonoBehaviour
     {
         puntos.text = GameManager.Instance.PuntosTotales.ToString();
     }
+    public void UpdatePoints(int puntosTotales)
+    {
+        puntos.text = puntosTotales.ToString();
+    }
+
+    public void FinishHeart(int indice)//INDICE QUE LE PASMOS
+    {
+        heart[indice].SetActive(false);
+    }
+
+    public void EarntHeart(int indice)
+    {
+
+        heart[indice].SetActive(true);
+    }
+
 }
