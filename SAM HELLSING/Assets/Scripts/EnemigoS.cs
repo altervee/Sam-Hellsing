30134d5250f5dc357574bbdc00dc4fd795aa6b56
Ivan <120404084+altervee@users.noreply.github.com>
2024-05-15 14:13:46 +0200

    Changes
    I fix tow errors with slimes' script and I added plataforms  in the first map

--- SAM HELLSING/Assets/Scripts/EnemigoS.cs
+++ SAM HELLSING/Assets/Scripts/EnemigoS.cs
@@ -17,6 +17,7 @@ public class EnemigoS : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private bool puedeAtacar = true;
     private bool estaVivo = true;
+    public AudioClip sonidMovimientoSlime;
 
     void Start()
     {
@@ -30,7 +31,7 @@ public class EnemigoS : MonoBehaviour
     {
         // Mirar hacia el jugador
         Vector2 direccionJugador = jugador.position - transform.position;
-        if (direccionJugador.magnitude <= distanciaVision)
+        if ((direccionJugador.magnitude <= distanciaVision)&& estaVivo)
         {
             // Establecer la orientación del sprite
             if (direccionJugador.x > 0)
@@ -46,6 +47,7 @@ public class EnemigoS : MonoBehaviour
 
             // Si el jugador está dentro de la distancia de visión, seguir al jugador
             animator.SetBool("Sigue", true);
+            
         }
         else
         {
@@ -62,38 +64,17 @@ public class EnemigoS : MonoBehaviour
     {
         if (!estaVivo)
             return;
-
+        AudioManager.Instance.ReproducirSonido(sonidMovimientoSlime);
         vida -= daño;
 
         if (vida <= 0)
         {
+
             Muerte();
         }
     }
 
     // Método para la animación de muerte y destrucción del enemigo
-    private void Muerte()
-    {
-        estaVivo = false;
-
-        // Activar la animación de muerte
-        animator.SetTrigger("Muerte");
-
-        // Desactivar componentes para evitar interacciones después de muerto
-        GetComponent<Rigidbody2D>().simulated = false;
-        GetComponent<Collider2D>().enabled = false;
-
-        // Retrasar la destrucción del objeto después de un cierto tiempo
-        float tiempoDeEspera = 2.0f; // Cambia esto al tiempo que desees
-        Invoke("DestruirObjeto", tiempoDeEspera);
-    }
-
-    // Método para destruir el objeto después de la animación de muerte
-    private void DestruirObjeto()
-    {
-        // Destruir el objeto después de que haya pasado el tiempo de espera
-        Destroy(gameObject);
-    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -104,16 +85,39 @@ public class EnemigoS : MonoBehaviour
                 return;
 
             puedeAtacar = false;
+            Color color = spriteRenderer.color;
+            color.a = 0.5f;
+            spriteRenderer.color = color;
 
-            // Hacer daño al jugador
+            GameManager.Instance.PerderVida();
+
             other.gameObject.GetComponent<playerControler>().AplicarGolpe();
 
             Invoke("ReactivarAtaque", cooldownAtaque);
         }
     }
 
-    private void ReactivarAtaque()
+    void ReactivarAtaque()
     {
         puedeAtacar = true;
+
+        Color c = spriteRenderer.color;
+        c.a = 1f;
+        spriteRenderer.color = c;
+    }
+    private void Muerte()
+    {
+        // Activar la animación de muerte
+        animator.SetTrigger("Muerte");
+
+        // Retrasar la destrucción del objeto después de un cierto tiempo
+        float tiempoDeEspera = 2.0f; // Cambia esto al tiempo que desees
+        Invoke("DestruirObjeto", tiempoDeEspera);
+    }
+
+    private void DestruirObjeto()
+    {
+        // Destruir el objeto después de que haya pasado el tiempo de espera
+        Destroy(gameObject);
     }
 }
